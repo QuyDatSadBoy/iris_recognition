afb7e9760ad77c9b93a3f7bd267cbc674cb21256
QuyDatSadBoy <dattqyl09@gmail.com>
2025-04-22 14:29:44 +0700

    the first iris_recognition

---
+++ src/main/java/com/example/iris_recognition/IrisRecognitionApplication.java
@@ -0,0 +1,13 @@
+package com.example.iris_recognition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IrisRecognitionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IrisRecognitionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/iris_recognition/IrisRecognitionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.iris_recognition;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IrisRecognitionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

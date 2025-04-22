ea4919726cda89123494779dd381601595cf101e
QuyDatSadBoy <dattqyl09@gmail.com>
2025-04-22 14:34:17 +0700

    the first iris_recognition dat

--- src/main/java/com/example/iris_recognition/IrisRecognitionApplication.java
+++ src/main/java/com/example/iris_recognition/IrisRecognitionApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class IrisRecognitionApplication {
 
     public static void main(String[] args) {
+        System.out.println("IrisRecognitionApplication");
         SpringApplication.run(IrisRecognitionApplication.class, args);
     }
+
 
 }
